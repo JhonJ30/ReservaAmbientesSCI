e0745b89f23649427a8da080494ce07cc9962caa
BeatrizaRios <201902491@est.umss.edu>
2024-03-31 16:16:30 -0400

    Se borro fiwi

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,23 +1,20 @@
 <link href="{{asset ('css/search.css')}}" rel="stylesheet">
 
-<form action="/client/buscarAmbientes" method="GET">
 <div class="searchBox">
-    <input class="search" type="text" name="search" spellcheck="false" placeholder="Nombre del Aula, Ubicación...">
+    <input class="search" type="text" name="search" spellcheck="false" placeholder="no Aula, Descripción">
     <div class="icon">
-        <button class="iconButton" type="submit">
+        <button class="iconButton"  onclick="openModal()">
             <i class="fas fa-search"></i>
         </button>
     </div>
-    <div class="icon">
-        <button class="iconButton" type="button" onclick="openModal()">
+    <div class="">
+        <button class="iconButton" onclick="openModal()">
             <i class="fas fa-sliders"></i>
         </button>
     </div>
 </div>
-</form>
 
 <dialog class="modal" id="mainModal">
-    <form action="/client/buscarAmbientesAvanzado" method="GET">
     <div class="contentSearch">
         <div class="filter">
             <label>Fecha: </label>
@@ -28,38 +25,34 @@
         </div>
 
         <div class="filter">
-                <label>Recursos: </label>
-                <div class="selectResource" style="margin-left: 2%;">
-                    <div class="resource">
-                        <input type="checkbox" name="recursos[]" id="resource1" value="Proyector">
-                        <label for="resource1">Proyector</label>
-                    </div>
-                    <div class="resource">
-                        <input type="checkbox" name="recursos[]" id="resource2" value="Pizarra">
-                        <label for="resource2">Pizarra</label>
-                    </div>
-                    <div class="resource">
-                        <input type="checkbox" name="recursos[]" id="resource3" value="WiFi">
-                        <label for="resource3">WiFi</label>
-                    </div>
-                    <div class="resource">
-                        <input type="checkbox" name="recursos[]" id="resource4" value="Otros">
-                        <label for="resource4">Otros</label>
-                    </div>
-                    <!-- Agrega más checkboxes si es necesario -->
+            <label>Recursos: </label>
+            <div class="selectResource" style="margin-left: 2%;">
+                <div class="resource">
+                    <input type="checkbox" id="resource1" value="Proyector">
+                    <label for="resource1">Proyector</label>
+                </div>
+                <div class="resource">
+                    <input type="checkbox" id="resource2" value="Proyector">
+                    <label for="resource2">Pizarra</label>
+                </div>
+                <div class="resource">
+                    <input type="checkbox" id="resource2" value="Proyector">
+                    <label for="resource2">Otros </label>
                 </div>
             </div>
-            <div class="filter">
-                <label>Capacidad: </label>
-                <input class="minValue" type="number" name="minValue" min="0" max="200" placeholder="min">
-                <input class="maxValue" type="number" name="maxValue" min="0" max="200" placeholder="max">
-            </div>
         </div>
-        <button class="cancelButton" type="button" onclick="closeModal()">Cancelar</button>
-        <button class="searchButton" type="submit">Buscar</button>
+
+
+        <div class="filter">
+            <label>Capacidad: </label>
+            <input class="minValue" type="number" min="0" max="200" placeholder="min">
+            <input class="maxValue" type="number" min="0" max="200" placeholder="max">
+        </div>
     </div>
-    </form>
+
+
+    <button class="cancelButton" onclick="closeModal()">Cancelar</button>
+    <button class="searchButton" onclick="closeModal()">Buscar</button>
 </dialog>
-</form>
 
 <script src="{{asset ('js/search.js')}}"></script>
