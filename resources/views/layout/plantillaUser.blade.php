d7bcf0c24b39f0d6427493869857683443745e5d
Maribel Jamira <maribeljamiram@gmail.com>
2024-04-05 07:31:34 -0400

    modificado por mary

--- resources/views/layout/plantillaUser.blade.php
+++ resources/views/layout/plantillaUser.blade.php
@@ -10,28 +10,33 @@
 </head>
 <!--menu usuario-->
 <body>
-    <div class="div1" id="div1">
-        <img src="{{asset ('img/san simon.png')}}" class="logo" alt="...">
-        <nav class="navMenu">
-            <div class="menu">
-                <ul>
-                    <li><a href="/client" class="priHabilitado"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
-                    <li><a href="/client/verAmbientes" class="priHabilitado2"><i class="fas fa-eye"></i>&nbsp;Ver Ambientes</a></li>
-                    <li><a href="/client/ayuda" class="ultimo"><i class="fas fa-question-circle"></i>&nbsp;Ayuda</a></li>
-                </ul>
-                <ul>
-                    <li class="sesion">
-                        <a href="/" class='iniSesion'><i class="fas fa-user"></i>&nbsp;Mi Cuenta</a>
-                    </li>
+    
 
-                </ul>
+    <div class="navbar">
+            <div class="logo">
+            <img src="{{asset ('img/san simon.png')}}" class="logo" alt="...">
             </div>
-        </nav>
-
+            <div class="menu-toggle" onclick="toggleMenu()">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+            <ul class="menu">
+                <li> <a href="/client" class="priHabilitado"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
+                <li><a href="/client/verAmbientes" class="priHabilitado2"><i class="fas fa-eye"></i>&nbsp;Visualizar</a></li>
+                <li><a href="/client/ayuda" class="ultimo" ><i class="fas fa-question-circle"></i>&nbsp;Ayuda</a></li>
+                <li><a href="/" class='iniSesion'><i class="fas fa-user"></i>Mi Cuenta</a></li>
+            </ul>
     </div>
     <div class="container">
         @yield('contenido')
     </div>
+    <script>
+        function toggleMenu() {
+            const menu = document.querySelector('.menu');
+            menu.classList.toggle('active');
+        }
+        </script>
 </body>
 
 </html>
