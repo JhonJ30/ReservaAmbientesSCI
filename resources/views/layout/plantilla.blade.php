42ee56eb8bab9b4bf396e5c45241816ed70c4c1d
leonel1236xd <leonelferrel03@gmail.com>
2024-04-05 23:14:53 -0400

    Cambios para que se puedan ver lo de registrar y visualizar aulas en responsivo

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -21,14 +21,14 @@
   </div>
   <ul class="menu">
     <li> <a href="/" class="priHabilitado"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
-    <li><a href="#" class="priHabilitado2"><i class="fas fa-plus-circle"></i>&nbsp;Registrar</a>
-      <ul class="submenu">
+    <li><a href="#" id="menuRegistrar" class="priHabilitado2"><i class="fas fa-plus-circle"></i>&nbsp;Registrar</a>
+      <ul class="submenu" id="submenuRegistrar">
         <p><a href="/registroAmb" class="priHabilitado2">&nbsp;&nbsp;Ambientes</a></p>
         <p><a href="/horarios" class="priHabilitado2">&nbsp;&nbsp;Horarios</a></p>
       </ul>
     </li>
-    <li><a href="#" class="priHabilitado2"><i class="fas fa-eye"></i>&nbsp;Visualizar</a>
-      <ul class="submenu">
+    <li><a href="#" id="menuRegistrar2" class="priHabilitado2"><i class="fas fa-eye"></i>&nbsp;Visualizar</a>
+      <ul class="submenu" id="submenuRegistrar2">
         <p><a href="/listaA" class="priHabilitado2">&nbsp;&nbsp;Ambientes</a></p>
         <p><a href="/listaH" class="priHabilitado2">&nbsp;&nbsp;Horarios</a></p>
       </ul>
@@ -46,6 +46,24 @@
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
   }
+
+  document.addEventListener('DOMContentLoaded', function () {
+  const menuRegistrar = document.getElementById('menuRegistrar');
+  menuRegistrar.addEventListener('click', function (event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del enlace.
+    const submenu = document.getElementById('submenuRegistrar');
+    submenu.classList.toggle('active'); // Muestra u oculta el submenú.
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuRegistrar = document.getElementById('menuRegistrar2');
+  menuRegistrar.addEventListener('click', function (event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del enlace.
+    const submenu = document.getElementById('submenuRegistrar2');
+    submenu.classList.toggle('active'); // Muestra u oculta el submenú.
+  });
+});
 </script>
 
 </body>
