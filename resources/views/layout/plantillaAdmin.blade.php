eb151f10f9202633d083dc749a5593c2deca1090
leonel1236xd <leonelferrel03@gmail.com>
2024-06-17 10:33:38 -0400

    ultimos bugs arreglados

--- resources/views/layout/plantillaAdmin.blade.php
+++ resources/views/layout/plantillaAdmin.blade.php
@@ -1,6 +1,8 @@
 @php
     $userId = auth()->id();
     $ambientes = App\Models\Reservar::where('codUser', $userId)->get();
+    use Carbon\Carbon;
+    $currentDate = Carbon::now();
 @endphp
 
 <!DOCTYPE html>
@@ -9,8 +11,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css>
-    <link href="{{asset ('css/home.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <title>Reservas-UMSS</title>
 </head>
 <body>
@@ -18,7 +20,7 @@
 <div class="main"> 
     <div class="navbar">
         <div class="logo">
-            <img src="{{asset ('img/san simon.png')}}" class="logo" alt="...">
+            <img src="{{ asset('img/san simon.png') }}" class="logo" alt="...">
             <span class="logo-text">RESERVA DE AMBIENTES</span>
         </div>
         <div class="menu-toggle" onclick="toggleMenu()">
@@ -27,7 +29,7 @@
             <div class="bar"></div>
         </div>
         <ul class="menu">
-            <li> <a href="/" class="priHabilitadoInicio"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
+            <li><a href="/" class="priHabilitadoInicio"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
             <li><a href="#" id="menuRegistrar" class="priHabilitado2"><i class="fas fa-plus-circle"></i>&nbsp;Registrar</a>
                 <ul class="submenu" id="submenuRegistrar">
                     <p><a href="/registroAmbiente" class="priHabilitado2">&nbsp;&nbsp;Ambientes</a></p>
@@ -46,8 +48,7 @@
                     <p><a href="/listaUsuarios" class="priHabilitado2">&nbsp;&nbsp;Usuarios</a></p>
                 </ul>
             </li>
-            <li> <a href="/viewlistAviso" class="ultimo">&nbsp;Avisos</a></li>
-
+            <li><a href="/viewlistAviso" class="ultimo">&nbsp;Avisos</a></li>
             <div onmouseover="showMiCuentaContent()" onmouseout="hideMiCuentaContent()">
                 <li><a href="#" class='iniSesion'><i class="fas fa-user"></i>Mi Cuenta</a></li>
                 <div id="miCuentaContent" class="miCuentaContent" style="display: none;">
@@ -62,23 +63,28 @@
                         @else
                         <ul>
                             @foreach ($ambientes as $ambiente)
-                            @php
-                                $nombre = DB::table('ambiente')->where('id', $ambiente->codAmb)->first();
-                            @endphp
-                            <li>
-                                <span>Aula: {{ $nombre->nroAmb }}<br>
-                                    Fecha: {{ $ambiente->fecha }}<br>
-                                    Hora: {{ $ambiente->horaInicio }}<br>
-                                    Estado: {{ $ambiente->estado }}
-                                </span>
-                                @if ($ambiente->estado == 'Proceso')
-                           <a href="{{ route('editarReserva', $ambiente->id) }}" class="editar-button" style="color:#ffff; font-weight: normal;">Modificar</a>
-                           <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
-                         @elseif ($ambiente->estado == 'Aceptado' || $ambiente->estado == 'Rechazado')
-                           <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
-                         @endif
+                                @php
+                                    $nombre = DB::table('ambiente')->where('id', $ambiente->codAmb)->first();
+                                    $ambienteDate = Carbon::parse($ambiente->fecha);
+                                @endphp
 
-                            </li>
+                                @if ($ambienteDate >= $currentDate)
+                                    <li>
+                                        <span>Aula: {{ $nombre->nroAmb }}<br>
+                                            Fecha: {{ $ambiente->fecha }}<br>
+                                            Hora: {{ $ambiente->horaInicio }}<br>
+                                            Estado: {{ $ambiente->estado }}
+                                        </span>
+                                        @if ($ambiente->estado == 'Proceso')
+                                            <a href="{{ route('editarReserva', $ambiente->id) }}" class="editar-button" style="color:#ffff; font-weight: normal;">Modificar</a>
+                                            <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
+                                        @elseif ($ambiente->estado == 'Aceptado')
+                                            <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
+                                        @elseif ($ambiente->estado == 'Rechazado')
+                                        
+                                        @endif
+                                    </li>
+                                @endif
                             @endforeach
                         </ul>
                         @endif
@@ -87,65 +93,66 @@
                         </button>
                     </div>
                 </div>
+            </div>
         </ul>
     </div>
  
     @if(Session::has('success'))
-<div id="successModal" class="modal" style="display: block; background-color: rgba(0, 0, 0, 0.5); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 999;">
-  <div class="modal-content" style="background-color: white; width: 26%; margin: 20% auto; padding: 20px; border-radius: 5px; text-align: center;">
-    <p><strong>{{ Session::get('success') }}</strong></p>
-    <!-- Otro contenido del modal si es necesario -->
-    <div class="button-container">
-      <button class="btnAceptar" style="background-color: #0B6F63; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;" onclick="closeSuccessModal()">Aceptar</button>
+    <div id="successModal" class="modal" style="display: block; background-color: rgba(0, 0, 0, 0.5); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 999;">
+        <div class="modal-content" style="background-color: white; width: 26%; margin: 20% auto; padding: 20px; border-radius: 5px; text-align: center;">
+            <p><strong>{{ Session::get('success') }}</strong></p>
+            <div class="button-container">
+                <button class="btnAceptar" style="background-color: #0B6F63; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;" onclick="closeSuccessModal()">Aceptar</button>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-@endif
+    @endif
    
     <div class="container">
-        @yield('contenido')f
+        @yield('contenido')
     </div>
 </div>    
 </body>
 
 <script>
-        function toggleMenu() {
-            const menu = document.querySelector('.menu');
-            menu.classList.toggle('active');
-        }
+    function toggleMenu() {
+        const menu = document.querySelector('.menu');
+        menu.classList.toggle('active');
+    }
 
-        document.addEventListener('DOMContentLoaded', function() {
-            const menuRegistrar = document.getElementById('menuRegistrar');
-            menuRegistrar.addEventListener('click', function(event) {
-                event.preventDefault(); // Previene el comportamiento por defecto del enlace.
-                const submenu = document.getElementById('submenuRegistrar');
-                submenu.classList.toggle('active'); // Muestra u oculta el submenú.
-            });
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuRegistrar = document.getElementById('menuRegistrar');
+        menuRegistrar.addEventListener('click', function(event) {
+            event.preventDefault(); // Previene el comportamiento por defecto del enlace.
+            const submenu = document.getElementById('submenuRegistrar');
+            submenu.classList.toggle('active'); // Muestra u oculta el submenú.
         });
+    });
 
-        document.addEventListener('DOMContentLoaded', function() {
-            const menuRegistrar = document.getElementById('menuRegistrar2');
-            menuRegistrar.addEventListener('click', function(event) {
-                event.preventDefault(); // Previene el comportamiento por defecto del enlace.
-                const submenu = document.getElementById('submenuRegistrar2');
-                submenu.classList.toggle('active'); // Muestra u oculta el submenú.
-            });
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuRegistrar2 = document.getElementById('menuRegistrar2');
+        menuRegistrar2.addEventListener('click', function(event) {
+            event.preventDefault(); // Previene el comportamiento por defecto del enlace.
+            const submenu = document.getElementById('submenuRegistrar2');
+            submenu.classList.toggle('active'); // Muestra u oculta el submenú.
         });
+    });
 
-        document.getElementById('cancel-button2').addEventListener('click', function() {
-            window.location.href = "/logout";
-        });
+    document.getElementById('cancel-button2').addEventListener('click', function() {
+        window.location.href = "/logout";
+    });
 
-        function showMiCuentaContent() {
-            document.getElementById("miCuentaContent").style.display = "block";
-        }
+    function showMiCuentaContent() {
+        document.getElementById("miCuentaContent").style.display = "block";
+    }
 
-        function hideMiCuentaContent() {
-            document.getElementById("miCuentaContent").style.display = "none";
-        }
-        function closeSuccessModal() {
-    document.getElementById('successModal').style.display = 'none';
-  }
-    </script>
-   
+    function hideMiCuentaContent() {
+        document.getElementById("miCuentaContent").style.display = "none";
+    }
+    
+    function closeSuccessModal() {
+        document.getElementById('successModal').style.display = 'none';
+    }
+</script>
+
 </html>
--- resources/views/layout/plantillaDocente.blade.php
+++ resources/views/layout/plantillaDocente.blade.php
@@ -1,6 +1,8 @@
 @php
     $userId = auth()->id();
     $ambientes = App\Models\Reservar::where('codUser', $userId)->get();
+    use Carbon\Carbon;
+    $currentDate = Carbon::now();
 @endphp
 
 <!DOCTYPE html>
@@ -9,15 +11,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css>
-    <link href="{{asset ('css/homeUser.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="{{ asset('css/homeUser.css') }}" rel="stylesheet">
     <title>@yield('titulopagina')</title>
 </head>
 
 <body>
     <div class="navbar">
         <div class="logo">
-            <img src="{{asset ('img/san simon.png')}}" class="logo" alt="...">
+            <img src="{{ asset('img/san simon.png') }}" class="logo" alt="...">
         </div>
         <div class="menu-toggle" onclick="toggleMenu()">
             <div class="bar"></div>
@@ -25,10 +27,10 @@
             <div class="bar"></div>
         </div>
         <ul class="menu">
-            <li> <a href="/" class="priHabilitadoInicio"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
+            <li><a href="/" class="priHabilitadoInicio"><i class="fas fa-home"></i>&nbsp;Inicio</a></li>
             <li><a href="/verAmbientes" class="priHabilitado2"><i class="fas fa-plus-circle"></i>&nbsp;Ver Ambientes</a></li>
-            <li><a href="/notificacion" id="notificacionLink"  class="ultimo" ><i class="fas fa-bell"><sup id="contadorNotificaciones" style="color: red;">
-            {{ App\Http\Controllers\notificationController::contarNotificacionesRecientes() }}</sup></i>
+            <li><a href="/notificacion" id="notificacionLink" class="ultimo"><i class="fas fa-bell"><sup id="contadorNotificaciones" style="color: red;">
+                {{ App\Http\Controllers\notificationController::contarNotificacionesRecientes() }}</sup></i>
             </a></li>
 
             <div onmouseover="showMiCuentaContent()" onmouseout="hideMiCuentaContent()">
@@ -41,28 +43,34 @@
                         </div>
                         <h2>Reservas</h2>
                         @if ($ambientes->isEmpty())
-                        <p style="font-size: 20px; color: gray;">No hay reservas.</p>
+                            <p style="font-size: 20px; color: gray;">No hay reservas.</p>
                         @else
-                        <ul>
-                            @foreach ($ambientes as $ambiente)
-                            @php
-                                $nombre = DB::table('ambiente')->where('id', $ambiente->codAmb)->first();
-                            @endphp
-                            <li>
-                                <span>Aula: {{ $nombre->nroAmb }}<br>
-                                    Fecha: {{ $ambiente->fecha }}<br>
-                                    Hora: {{ $ambiente->horaInicio }}<br>
-                                    Estado: {{ $ambiente->estado }}
-                                </span>
-                                @if ($ambiente->estado == 'Proceso')
-                           <a href="{{ route('editarReserva', $ambiente->id) }}" class="editar-button" style="color:#ffff; font-weight: normal;">Modificar</a>
-                           <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
-                         @elseif ($ambiente->estado == 'Aceptado' || $ambiente->estado == 'Rechazado')
-                           <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
-                         @endif
-                            </li>
-                            @endforeach
-                        </ul>
+                            <ul>
+                                @foreach ($ambientes as $ambiente)
+                                    @php
+                                        $nombre = DB::table('ambiente')->where('id', $ambiente->codAmb)->first();
+                                        $ambienteDate = Carbon::parse($ambiente->fecha);
+                                    @endphp
+
+                                    @if ($ambienteDate >= $currentDate)
+                                        <li>
+                                            <span>Aula: {{ $nombre->nroAmb }}<br>
+                                                Fecha: {{ $ambiente->fecha }}<br>
+                                                Hora: {{ $ambiente->horaInicio }}<br>
+                                                Estado: {{ $ambiente->estado }}
+                                            </span>
+                                            @if ($ambiente->estado == 'Proceso')
+                                                <a href="{{ route('editarReserva', $ambiente->id) }}" class="editar-button" style="color:#ffff; font-weight: normal;">Modificar</a>
+                                                <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
+                                            @elseif ($ambiente->estado == 'Aceptado')
+                                                <a href="{{ route('eliminarReserva', $ambiente->id) }}" class="cancel-button" style="color:#ffff; font-weight: normal;">Cancelar</a>
+                                            @elseif ($ambiente->estado == 'Rechazado')
+                                                
+                                            @endif
+                                        </li>
+                                    @endif
+                                @endforeach
+                            </ul>
                         @endif
                         <div class="footer">
                             <button class="cancel-button2" id="cancel-button2">Cerrar Sesión
@@ -75,12 +83,11 @@
     </div>
 
     <!-- Modal de confirmación de eliminación -->
-    <div id="myMod-al" class="modal">
-
+    <div id="myModal" class="modal">
         <div class="modal-content">
             <p><strong>¿Estás seguro que quiere cancelar su Reserva?</strong></p>
             <br>
-            <p class="gris">Esta operacion es irreversible</p>
+            <p class="gris">Esta operación es irreversible</p>
             <br>
             <div class="button-container">
                 <form method="POST">
@@ -93,20 +100,20 @@
             </div>
         </div>
     </div>
+    
     <div class="container">
         @yield('contenido')
     </div>
 </body>
-   
+
 @if(Session::has('success'))
 <div id="successModal" class="modal" style="display: block; background-color: rgba(0, 0, 0, 0.5); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 999;">
-  <div class="modal-content" style="background-color: white; width: 50%; margin: 20% auto; padding: 20px; border-radius: 5px; text-align: center;">
-    <p><strong>{{ Session::get('success') }}</strong></p>
-    <!-- Otro contenido del modal si es necesario -->
-    <div class="button-container">
-      <button class="btnAceptar" style="background-color: #0B6F63; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;" onclick="closeSuccessModal()">Aceptar</button>
+    <div class="modal-content" style="background-color: white; width: 50%; margin: 20% auto; padding: 20px; border-radius: 5px; text-align: center;">
+        <p><strong>{{ Session::get('success') }}</strong></p>
+        <div class="button-container">
+            <button class="btnAceptar" style="background-color: #0B6F63; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;" onclick="closeSuccessModal()">Aceptar</button>
+        </div>
     </div>
-  </div>
 </div>
 @endif
 
@@ -152,29 +159,22 @@
         document.getElementById("miCuentaContent").style.display = "none";
     }
 
-    // Función para abrir el modal
     function openModal2(registroId) {
         document.getElementById('registro_id').value = registroId;
-        console.log(registroId);
-        // Mostrar el modal
         document.getElementById('myModal').style.display = 'block';
     }
 
-    // Función para cerrar el modal
     function closeModal2() {
         document.getElementById('myModal').style.display = 'none';
     }
 
-    //notificacion
-    
     document.getElementById('notificacionLink').addEventListener('click', function() {
         document.getElementById('contadorNotificaciones').textContent = '0';
     });
 
     function closeSuccessModal() {
-    document.getElementById('successModal').style.display = 'none';
-  }
-
+        document.getElementById('successModal').style.display = 'none';
+    }
 </script>
 
 </html>
